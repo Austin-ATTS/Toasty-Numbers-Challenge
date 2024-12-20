924429e2eb0e667de6bcbb55e63d60062cab8485
AustinWelsh-Graham <Austin@austinatts.co.uk>
2024-12-20 21:38:24 +0000

    V0.0.4

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
+from kivy.core.window import Window
+from screens.home import Home
+from screens.rounds_mode import RoundsMode
+from screens.endless_mode import EndlessMode
+from screens.settings import Settings
+from screens.accounts import Account
+from screens.leaderboard import Leaderboard
 
 class ToastyNumbersApp(App):
     def build(self):
